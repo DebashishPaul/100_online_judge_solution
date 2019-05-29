0425ee58e48f78e142532d7e1d160e4b38d9be38
DebashishPaul <debashish333paul@gamil.com>
2019-05-29 21:44:29 +0600

    online judge

---
+++ problem1.py
@@ -0,0 +1,8 @@
+val = int(input("Enter your first value: "))
+val2=int(input("enter your second value: "))
+x=200
+if val<x and val2<x:
+	sum=val+val2
+	print(sum)
+else:
+    print("enter the value less than 200")
