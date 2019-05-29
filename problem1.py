d0272e73b9b5eb81c3f8fed03269c6de8c84cc52
Debashish Paul <debashish333paul@gmail.com>
2019-05-29 21:47:13 +0600

    Add files via upload

--- problem1.py
+++ problem1.py
@@ -1,3 +1,15 @@
+'''
+Given two natural numbers (both not greater than 200), each number in the separate line, please print the sum of them.
+
+Example
+Input:
+2
+3
+
+Output:
+5
+'''
+
 val = int(input("Enter your first value: "))
 val2=int(input("enter your second value: "))
 x=200
